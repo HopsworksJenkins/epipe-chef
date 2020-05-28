bf33427f8b402abb8b3bd6b2eef319efc08c7603
Alex Ormenisan <aaor@kth.se>
2020-05-28 23:51:13 +0200

    test

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ include_attribute "elasticsearch"
 
 default['epipe']['version']                  = "0.13.0"
 default['epipe']['user']                     = node['install']['user'].empty? ? node['hops']['hdfs']['user'] : node['install']['user']
-default['epipe']['url']                      = "#{node['download_url']}/epipe/#{node['platform_family']}/epipe-#{node['epipe']['version']}.tar.gz"
+default['epipe']['url']                      = "#{node['download_url']}/epipe/test/#{node['platform_family']}/epipe-#{node['epipe']['version']}-pr-67.tar.gz"
 default['epipe']['systemd']                  = "true"
 default['epipe']['dir']                      = node['install']['dir'].empty? ? "/srv" : node['install']['dir']
 default['epipe']['home']                     = node['epipe']['dir'] + "/epipe-" + "#{node['epipe']['version']}"
